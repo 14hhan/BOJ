ced67426b921209bab14cb720dc32e39c62b481f
14hhan <14hhan@gmail.com>
2021-03-24 01:13:29 +0900

    Create 11721.cpp

---
+++ 11721.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  string str;
+  cin >> str;
+
+  for (int i = 1; i <= str.length(); i++) {
+    cout << str[i-1];
+    if (i % 10 == 0) cout << '\n';
+  }
+}
